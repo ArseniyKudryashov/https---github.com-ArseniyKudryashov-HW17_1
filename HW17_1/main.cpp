714d8a8833ac6443d5e58109589a6a1dbbc6c8be
kudryashovarseniy <arsen2008kudr@gmail.com>
2024-01-16 21:34:38 +0200

    Количество работников старше 60-ти лет

--- HW17_1/main.cpp
+++ HW17_1/main.cpp
@@ -14,6 +14,18 @@ int main()
         {"Yellow", 1985, "Accountant", 60000, "Higher"},
         {"Brown", 1967, "Manager", 80000, "Secondary"}
     };
-
     
+    int count = 0;
+    cout << "Employees older than 60 years:" << endl;
+    for (const auto& employee : employees)
+    {
+        if (2024 - employee.yearBirth > 60)
+        {
+            cout << "Surname: " << employee.surname << ", Year of birthday: " << employee.yearBirth
+            << ", Job title : " << employee.jobTitle << ", Salary: " << employee.salary
+            << ", Education: " << employee.education << "\n";
+            count++;
+        }
+    }
+    cout << endl << "Total employees older than 60 years: " << count << endl;
 }
